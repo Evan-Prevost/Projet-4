0669dcb6001479dc326da036ea677b5fd5030607
Evan-Prevost <115547264+Evan-Prevost@users.noreply.github.com>
2022-11-14 15:22:43 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "biberon.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Good morning USA !\n";
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
